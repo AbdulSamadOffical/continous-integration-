f9c970ca9b836589448c3a5d5f2ccf9e686c456c
Abdul samad <studyonlinetech47@gmail.com>
2022-01-01 12:07:05 +0500

    added a period

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("GET /", () => {
-  it("respond with Continous integration", (done) => {
-    request(app).get("/").expect("Continous integration", done);
+  it("respond with Continous integration.", (done) => {
+    request(app).get("/").expect("Continous integration.", done);
   });
 });
