53ceba17281fc76fa3293ad19140267608540f99
Abdul samad <studyonlinetech47@gmail.com>
2022-01-07 19:17:27 +0500

    port has been changed and also the message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
-  return res.send("Continous integration.");
+  return res.json({ message: "Check your cicd pipeline" });
 });
 
 // listen to port 7000 by default
