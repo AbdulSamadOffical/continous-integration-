fa3d9a18e21297b68dd6a1d5476206f59fe49028
Abdul samad <studyonlinetech47@gmail.com>
2022-01-07 19:39:24 +0500

    new commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
-  return res.send("HelloWorld");
+  return res.send("test the app");
 });
 
 // listen to port 7000 by default
